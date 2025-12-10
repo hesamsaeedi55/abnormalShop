939d8e12102b371c7855db215b711c61d1aab21d
Hesam <118046088+hesamsaeedi55@users.noreply.github.com>
2025-12-10 08:55:48 +0330

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 import os
 from django.core.management.utils import get_random_secret_key
 
-# Use environment variable if set, otherwise generate a random key
-SECRET_KEY = os.environ.get("d3d411f23ba7be8d9700fb20072bac6c", get_random_secret_key())
+SECRET_KEY = os.environ.get("SECRET_KEY", get_random_secret_key())
